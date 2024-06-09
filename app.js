4a58bbd56c7a56ac6ee9a88067df3e4568ad4dcb
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 12:12:37 -0400

    gen error handler

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
 module.exports = app;
