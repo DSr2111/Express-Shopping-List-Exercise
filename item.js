72cdb27ea9f09c04c4ea1ea99b7c4229cb7dbff9
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 12:33:49 -0400

    class WIP static meth

---
+++ item.js
@@ -0,0 +1,13 @@
+const items = require("./fakeDb");
+
+class Item {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    items.push(this);
+  }
+
+  static findAll() {
+    return items;
+  }
+}
