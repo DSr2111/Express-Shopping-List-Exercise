15a79160d691559a349fa449a6ce1d13616aef5b
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 14:50:59 -0400

    test fin

--- routes/items.test.js
+++ routes/items.test.js
@@ -16,6 +16,7 @@ beforeEach(async () => {
 afterEach(async () => {
   items = [];
 });
+/** GET /items/[name] - return data about one item: `{item: item}` */
 
 describe("GET /items/:name", async function () {
   test("Gets a single item", async function () {
@@ -29,3 +30,53 @@ describe("GET /items/:name", async function () {
     expect(response.statusCode).toBe(404);
   });
 });
+// end
+
+/** POST /items - create item from data; return `{item: item}` */
+
+describe("POST /items", async function () {
+  test("Creates a new item", async function () {
+    const response = await request(app).post(`/items`).send({
+      name: "Taco",
+      price: 0,
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.item).toHaveProperty("name");
+    expect(response.body.item).toHaveProperty("price");
+    expect(response.body.item.name).toEqual("Taco");
+    expect(response.body.item.price).toEqual(0);
+  });
+});
+// end
+
+/** PATCH /items/[name] - update item; return `{item: item}` */
+
+describe("PATCH /items/:name", async function () {
+  test("Updates a single item", async function () {
+    const response = await request(app).patch(`/items/${item.name}`).send({
+      name: "Troll",
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.item).toEqual({
+      name: "Troll",
+    });
+  });
+
+  test("Responds with 404 if can't find item", async function () {
+    const response = await request(app).patch(`/items/0`);
+    expect(response.statusCode).toBe(404);
+  });
+});
+// end
+
+/** DELETE /items/[name] - delete item,
+ *  return `{message: "item deleted"}` */
+
+describe("DELETE /items/:name", async function () {
+  test("Deletes a single a item", async function () {
+    const response = await request(app).delete(`/items/${item.name}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ message: "Deleted" });
+  });
+});
+// end
