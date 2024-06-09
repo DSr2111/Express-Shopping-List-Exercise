8b66de26e87c8bcbfea291ad6d0d5b92a1b4d5cc
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 14:20:38 -0400

    get route

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,13 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get();
+router.get("", (req, res, next) => {
+  try {
+    return res.json({ items: Item.findAll() });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 router.post();
 
